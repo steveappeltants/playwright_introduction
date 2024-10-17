db26d73f2cbdc09b0379ff1aeb56902d2eaf6a62
Nele Custers <custersnele@gmail.com>
2024-10-17 10:54:56 +0200

    add data

---
+++ web/app/js/events.js
@@ -0,0 +1,30 @@
+// Global list of events
+const events = [
+	{
+		id: 1,
+		image: "img/shutterstock_415922566_thumbnail_1.jpg",
+		date: "1/11/2024 4:22 PM",
+		name: "Alexander Lemtov Live",
+		artist: "Alexander Lemtov",
+		price: "$65",
+		description: "Alexander Lemtov's live show will bring you unforgettable experiences!"
+	},
+	{
+		id: 2,
+		image: "img/shutterstock_606456248_thumbnail_2.jpg",
+		date: "3/11/2024 4:22 PM",
+		name: "To the Moon and Back",
+		artist: "Santiago Martinez",
+		price: "$135",
+		description: "Experience the musical journey to the moon and beyond with Santiago Martinez."
+	},
+	{
+		id: 3,
+		image: "img/shutterstock_1746589040_thumbnail_3.jpg",
+		date: "4/11/2024 4:22 PM",
+		name: "The State of Affairs: Mariam Live!",
+		artist: "Mariam Johnson",
+		price: "$85",
+		description: "Join Mariam Johnson in a night of stories, laughter, and great music."
+	}
+];
