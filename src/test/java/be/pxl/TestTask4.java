c6c8e49cca4940d6a18fa59f7f637f2b7f175a8c
Steve Appeltants <steve.appeltants@student.pxl.be>
2024-10-18 22:35:55 +0200

    Task 4 - complete

--- src/test/java/be/pxl/TestTask4.java
+++ src/test/java/be/pxl/TestTask4.java
@@ -1,26 +1,75 @@
 package be.pxl;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
 
-public class TestTask4 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class TestTask4 {
 
     private static Playwright playwright;
     private static Browser browser;
-    private final String URL = "";
+    private String URL = "http://localhost:5001/app/catalog.html";
 
     private BrowserContext context;
     private Page page;
 
     @AfterAll
-    static void closeBrowser(){
+    static void closeBrowser() {
         playwright.close();
     }
 
-//    @BeforeAll
-//    static void
+    @BeforeAll
+    static void lauchBrowser() {
+        playwright = Playwright.create();
+        BrowserType.LaunchOptions options = new BrowserType.LaunchOptions().setHeadless(true);
+        browser = playwright.chromium().launch(options);
+    }
+
+    @Test
+    void basketQuantity() {
+        try {
+            context = browser.newContext();
+            page = context.newPage();
+            page.navigate(URL);
+
+            // Act
+            page.getByTestId("purchase-button-1").click();
+            page.getByTestId("quantity").selectOption("3");
+            page.getByTestId("place-order").click();
+            page.getByTestId("quantity-dropdown").selectOption("4");
+            page.getByTestId("update-button").click();
+            page.getByTestId("event-total").click();
+            // Arrange
+            String ticketCountValue = page.locator("#ticket-count").innerText();
+            String eventTotal = page.locator(".event-total").innerText();
+            // Assert
+            assertEquals(ticketCountValue, "4");
+            assertEquals(eventTotal, "$260.00");
+            System.out.printf("%-35s %s%n", "Updated basket #ticket count: ",  ticketCountValue);
+            System.out.printf("%-35s %s%n", "Updated total event amount: ",  eventTotal);
+
+        } catch (Exception e) {
+            Assertions.fail("Test failed due to unexpected exception: " + e.getMessage());
+        }
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
